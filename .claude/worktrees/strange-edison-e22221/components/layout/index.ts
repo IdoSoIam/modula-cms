75e494321830196fd4be07b99fbef0c8ccadb9c2
IdoSoIam <gardenier.eloy30@gmail.com>
2026-04-27 16:19:50 +0200

    ok

--- .claude/worktrees/strange-edison-e22221/components/layout/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as LayoutFooter } from './Footer.vue'
-export { default as LayoutMobileMenu } from './MobileMenu.vue'
-export { default as LayoutNavigation } from './Navigation.vue'
-export { default as LayoutUserMenu } from './UserMenu.vue'
